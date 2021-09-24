17aa14d9caaff537acd37656b3a8865b2b93e967
Joonas Lehtimäki <joonas.lehtimaki@polarsquad.com>
2021-09-24 15:17:02 +0300

    seed functionality

--- pkg/toornament/team_test.go
+++ pkg/toornament/team_test.go
@@ -1,15 +1,13 @@
 package toornament
 
 import (
-	"fmt"
 	"log"
 	"testing"
 )
 
 func TestIntegrationParticipantGetTeam(t *testing.T) {
-	team, err := GetTeam("Polar Squad")
+	_, err := GetTeam("Polar Squad")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(team)
 }
